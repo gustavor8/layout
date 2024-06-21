49ab3b5899d2ecac6b9b546df2792570f5e08f26
Gustavo Rodrigues <gustavogrmc@gmail.com>
2024-06-21 11:25:17 -0300

    Deploy 01

--- src/Fragment/Menu/ListMenu/index.jsx
+++ src/Fragment/Menu/ListMenu/index.jsx
@@ -1,9 +1,14 @@
 import './style.css';
 
 function ListMenu({ list }) {
+
+  function clickLink (id){
+    window.alert("Você clicou no link " + id);
+
+  }
   return (
     <ul className="menu-items">
-      {list.map(item => (<li className="menu-item"><a key={item.id} href="#">{item.text}</a></li>))}
+      {list.map(item => (<li className="menu-item" onClick={()=>clickLink(item.id + 1)}><a key={item.id} href="#">{item.text}</a></li>))}
     </ul>
   )
 }
